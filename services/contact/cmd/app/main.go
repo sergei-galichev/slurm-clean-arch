5f90418239881c0f9429c8dc44b6694bdeb7ef36
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-15 15:20:15 +1000

    added some test code

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	host, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hostname: %s\n", host)
+	fmt.Printf("Home dir: %s\n", homeDir)
+	fmt.Printf("%s\n", os.Getenv("LANGUAGE"))
 }
