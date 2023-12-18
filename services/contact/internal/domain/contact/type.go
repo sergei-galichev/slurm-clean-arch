c72a11af65bfa49cd34394521b9374dc92892158
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-18 15:23:53 +1000

    added structure Contact, constructors and getters

--- services/contact/internal/domain/contact/type.go
+++ services/contact/internal/domain/contact/type.go
@@ -1,1 +1,156 @@
 package contact
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"slurm-clean-arch/pkg/type/email"
+	"slurm-clean-arch/pkg/type/gender"
+	"slurm-clean-arch/pkg/type/phonenumber"
+	"slurm-clean-arch/services/contact/internal/domain/contact/age"
+	"slurm-clean-arch/services/contact/internal/domain/contact/name"
+	"slurm-clean-arch/services/contact/internal/domain/contact/patronymic"
+	"slurm-clean-arch/services/contact/internal/domain/contact/surname"
+	"time"
+)
+
+var (
+	ErrPhoneNumberRequired = errors.New("phone number is required")
+)
+
+type Contact struct {
+	id         uuid.UUID
+	createdAt  time.Time
+	modifiedAt time.Time
+
+	phoneNumber phonenumber.PhoneNumber
+	email       email.Email
+
+	name       name.Name
+	surname    surname.Surname
+	patronymic patronymic.Patronymic
+
+	age age.Age
+
+	gender gender.Gender
+}
+
+func NewWithID(
+	id uuid.UUID,
+	createdAt time.Time,
+	modifiedAt time.Time,
+	phoneNumber phonenumber.PhoneNumber,
+	email email.Email,
+	name name.Name,
+	surname surname.Surname,
+	patronymic patronymic.Patronymic,
+	age age.Age,
+	gender gender.Gender,
+) (
+	*Contact,
+	error,
+) {
+	if phoneNumber.IsEmpty() {
+		return nil, ErrPhoneNumberRequired
+	}
+
+	if id == uuid.Nil {
+		id = uuid.New()
+	}
+	return &Contact{
+		id:          id,
+		createdAt:   createdAt.UTC(),
+		modifiedAt:  modifiedAt.UTC(),
+		phoneNumber: phoneNumber,
+		email:       email,
+		name:        name,
+		surname:     surname,
+		patronymic:  patronymic,
+		age:         age,
+		gender:      gender,
+	}, nil
+}
+
+func New(
+	phoneNumber phonenumber.PhoneNumber,
+	email email.Email,
+	name name.Name,
+	surname surname.Surname,
+	patronymic patronymic.Patronymic,
+	age age.Age,
+	gender gender.Gender,
+) (
+	*Contact,
+	error,
+) {
+	if phoneNumber.IsEmpty() {
+		return nil, ErrPhoneNumberRequired
+	}
+
+	var timeNow = time.Now().UTC()
+	return &Contact{
+		id:          uuid.New(),
+		createdAt:   timeNow,
+		modifiedAt:  timeNow,
+		phoneNumber: phoneNumber,
+		email:       email,
+		name:        name,
+		surname:     surname,
+		patronymic:  patronymic,
+		age:         age,
+		gender:      gender,
+	}, nil
+}
+
+func (c Contact) ID() uuid.UUID {
+	return c.id
+}
+
+func (c Contact) CreatedAt() time.Time {
+	return c.createdAt
+}
+
+func (c Contact) ModifiedAt() time.Time {
+	return c.modifiedAt
+}
+
+func (c Contact) Email() email.Email {
+	return c.email
+}
+
+func (c Contact) PhoneNumber() phonenumber.PhoneNumber {
+	return c.phoneNumber
+}
+
+func (c Contact) Name() name.Name {
+	return c.name
+}
+
+func (c Contact) Surname() surname.Surname {
+	return c.surname
+}
+
+func (c Contact) Patronymic() patronymic.Patronymic {
+	return c.patronymic
+}
+
+func (c Contact) FullName() string {
+	return fmt.Sprintf(
+		"%s %s %s",
+		c.surname,
+		c.name,
+		c.patronymic,
+	)
+}
+
+func (c Contact) Age() age.Age {
+	return c.age
+}
+
+func (c Contact) Gender() gender.Gender {
+	return c.gender
+}
+
+func (c Contact) Equal(contact Contact) bool {
+	return c.id == contact.id
+}
