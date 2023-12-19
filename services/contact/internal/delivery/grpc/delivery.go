388486f3b2b4788f67cc858545742d864692aab4
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-19 15:38:02 +1000

    added constructor to structure implemented UnimplementedContactServiceServer

--- services/contact/internal/delivery/grpc/delivery.go
+++ services/contact/internal/delivery/grpc/delivery.go
@@ -1,1 +1,33 @@
 package grpc
+
+import (
+	contact "slurm-clean-arch/services/contact/internal/delivery/grpc/interface"
+	"slurm-clean-arch/services/contact/internal/usecase"
+)
+
+type Delivery struct {
+	contact.UnimplementedContactServiceServer
+	ucContact usecase.Contact
+	ucGroup   usecase.Group
+
+	options Options
+}
+
+type Options struct{}
+
+func New(ucContact usecase.Contact, ucGroup usecase.Group, options Options) *Delivery {
+	var d = &Delivery{
+		ucContact: ucContact,
+		ucGroup:   ucGroup,
+	}
+
+	d.SetOptions(options)
+
+	return d
+}
+
+func (d *Delivery) SetOptions(options Options) {
+	if d.options != options {
+		d.options = options
+	}
+}
