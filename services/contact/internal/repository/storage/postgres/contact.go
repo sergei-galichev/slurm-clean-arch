45880b55145ea4e497d8144513f3d466366c7578
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-21 17:01:53 +1000

    added some code implementation. NOT FINISHED

--- services/contact/internal/repository/storage/postgres/contact.go
+++ services/contact/internal/repository/storage/postgres/contact.go
@@ -1,14 +1,62 @@
 package postgres
 
 import (
+	"context"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+	"slurm-clean-arch/pkg/columncode"
 	"slurm-clean-arch/pkg/queryparameter"
+	"slurm-clean-arch/pkg/tools/transaction"
 	"slurm-clean-arch/services/contact/internal/domain/contact"
 )
 
+var mappingSortContact = map[columncode.ColumnCode]string{
+	"id":          "id",
+	"fullName":    "full_name",
+	"phoneNumber": "phone_number",
+	"name":        "name",
+	"surname":     "surname",
+	"patronymic":  "patronymic",
+	"email":       "email",
+	"gender":      "gender",
+	"age":         "age",
+}
+
 func (r *Repository) CreateContact(contacts ...*contact.Contact) ([]*contact.Contact, error) {
-	// TODO implement me
-	panic("implement me")
+	ctx := context.Background()
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(ctx context.Context, t pgx.Tx) {
+		err = transaction.Finish(ctx, tx, err)
+	}(ctx, tx)
+
+	return nil, nil
+
+}
+
+func (r *Repository) createContactTx(
+	ctx context.Context,
+	tx pgx.Tx,
+	contacts ...*contact.Contact,
+) ([]*contact.Contact, error) {
+	if len(contacts) == 0 {
+		return []*contact.Contact{}, nil
+	}
+
+	//_, err := tx.CopyFrom(
+	//	ctx,
+	//	pgx.Identifier{"slurm", "contact"},
+	//	dao.CreateColumnContact,
+	//	r.toCopyFromSource(contacts...),
+	//)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	return contacts, nil
 }
 
 func (r *Repository) UpdateContact(
