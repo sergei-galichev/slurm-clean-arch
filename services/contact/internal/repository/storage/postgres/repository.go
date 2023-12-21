88a0384043a866f7b6452c49e389e057fdfc75be
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-21 16:59:35 +1000

    added options and migrations

--- services/contact/internal/repository/storage/postgres/repository.go
+++ services/contact/internal/repository/storage/postgres/repository.go
@@ -1,9 +1,17 @@
 package postgres
 
 import (
+	"context"
+	"fmt"
 	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/pressly/goose/v3"
+	"github.com/spf13/viper"
 )
+
+func init() {
+	viper.SetDefault("MIGRATIONS_DIR", "./services/contact/internal/repository/storage/postgres/migrations")
+}
 
 type Repository struct {
 	db      *pgxpool.Pool
@@ -11,9 +19,16 @@ type Repository struct {
 	options Options
 }
 
-type Options struct{}
+type Options struct {
+	DefaultLimit  uint64
+	DefaultOffset uint64
+}
 
-func New(db *pgxpool.Pool, options Options) *Repository {
+func New(db *pgxpool.Pool, options Options) (*Repository, error) {
+	if err := migrations(db); err != nil {
+		return nil, err
+	}
+
 	var r = &Repository{
 		db:     db,
 		genSQL: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar),
@@ -21,11 +36,36 @@ func New(db *pgxpool.Pool, options Options) *Repository {
 
 	r.SetOptions(options)
 
-	return r
+	return r, nil
 }
 
 func (r *Repository) SetOptions(options Options) {
+	if options.DefaultLimit == 0 {
+		options.DefaultLimit = 10
+	}
+
 	if r.options != options {
 		r.options = options
 	}
+}
+
+func migrations(pool *pgxpool.Pool) (err error) {
+	ctx := context.Background()
+	db, err := goose.OpenDBWithDriver("postgres", pool.Config().ConnConfig.ConnString())
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if closeErr := db.Close(); closeErr != nil {
+			err = closeErr
+			return
+		}
+	}()
+
+	dir := viper.GetString("MIGRATIONS_DIR")
+	goose.SetTableName("contact_version")
+	if err = goose.RunContext(ctx, "up", db, dir); err != nil {
+		return fmt.Errorf("goose %s error : %w", "up", err)
+	}
+	return nil
 }
