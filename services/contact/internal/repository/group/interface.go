5d7e41ba99cb2a7e65e7bedd3ee23b4bc9a6f228
Sergei Galichev <sergey.galichev@outlook.com>
2023-12-22 16:13:38 +1000

    added new interface to Group repo

--- services/contact/internal/repository/group/interface.go
+++ services/contact/internal/repository/group/interface.go
@@ -1,1 +1,11 @@
 package group
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+)
+
+type Group interface {
+	UpdateGroupsContactCountByFilters(ctx context.Context, tx pgx.Tx, ID uuid.UUID) error
+}
