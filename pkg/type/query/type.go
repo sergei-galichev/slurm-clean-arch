3f58b8d77e26d566bb3a754ae1bc4601da4ee045
sergei <sergey.galichev@outlook.com>
2023-12-25 00:36:06 +1000

    added struct Query and methods

---
+++ pkg/type/query/type.go
@@ -0,0 +1,52 @@
+package query
+
+import (
+	"github.com/gin-gonic/gin"
+	"slurm-clean-arch/pkg/type/sort"
+)
+
+type Query struct {
+	Sorts  sort.Sorts
+	Limit  uint64
+	Offset uint64
+}
+
+type SortOptions struct{}
+
+type Options struct {
+	Sorts SortsOptions
+}
+
+type SortsOptions map[string]SortOptions
+
+var (
+	keyForSort        = "sort"
+	defaultKeyForSort = ""
+	keyForLimit       = "limit"
+	keyForOffset      = "offset"
+)
+
+func ParseQuery(c *gin.Context, options Options) (*Query, error) {
+	sorts, err := parseSorts(c.DefaultQuery(keyForSort, defaultKeyForSort), options.Sorts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Query{
+		Sorts:  sorts,
+		Limit:  parseLimit(c.Query(keyForLimit)),
+		Offset: parseOffset(c.Query(keyForOffset)),
+	}, nil
+}
+
+func ParseSorts(c *gin.Context, options SortsOptions) (sort.Sorts, error) {
+	return parseSorts(c.DefaultQuery(keyForSort, defaultKeyForSort), options)
+}
+
+func ParseLimit(c *gin.Context) uint64 {
+	return parseLimit(c.Query(keyForLimit))
+}
+
+func ParseOffset(c *gin.Context) uint64 {
+	return parseOffset(c.Query(keyForOffset))
+}
