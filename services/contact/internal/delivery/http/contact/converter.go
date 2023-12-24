9df247361c1397c5fe684bc4cdb3c86d1a3b4dc8
sergei <sergey.galichev@outlook.com>
2023-12-25 00:37:32 +1000

    not complete

---
+++ services/contact/internal/delivery/http/contact/converter.go
@@ -0,0 +1,18 @@
+package contact
+
+//func ToContactResponse(response *domainContact.Contact) *ContactResponse {
+//	return &ContactResponse{
+//		Id:         response.ID().String(),
+//		CreatedAt:  response.CreatedAt(),
+//		ModifiedAt: response.ModifiedAt(),
+//		ShortContact: ShortContact{
+//			PhoneNumber: response.PhoneNumber().String(),
+//			Email:       response.Email(),
+//			Gender:      response.Gender(),
+//			Age:         uint8(response.Age()),
+//			Name:        response.Name().String(),
+//			Surname:     response.Surname().String(),
+//			Patronymic:  response.Patronymic().String(),
+//		},
+//	}
+//}
